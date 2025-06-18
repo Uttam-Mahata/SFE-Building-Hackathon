d1d14fc18806cc6a74c3d528cf304aedba8444b4
Uttam Mahata <uttambav@gmail.com>
2025-06-18 10:25:58 +0530

    chore: Update JitPack configuration for Java 21 compatibility

--- jitpack-build.gradle.kts
+++ jitpack-build.gradle.kts
@@ -7,16 +7,22 @@ plugins {
 }
 
 // Set project-wide Java compatibility
+println("Configuring Java 21 compatibility for all projects")
+println("Java Home: ${System.getProperty("java.home")}")
+println("Java Version: ${JavaVersion.current()}")
+
 allprojects {
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
             jvmTarget = "21"
+            println("Setting Kotlin JVM target to: 21")
         }
     }
     
     tasks.withType<JavaCompile>().configureEach {
         sourceCompatibility = JavaVersion.VERSION_21.toString()
         targetCompatibility = JavaVersion.VERSION_21.toString()
+        println("Setting Java compatibility to: ${JavaVersion.VERSION_21}")
     }
 }
 
